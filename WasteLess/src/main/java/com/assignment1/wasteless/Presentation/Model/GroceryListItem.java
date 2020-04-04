1268fd459a9775d7236fb1ec697bc6ffc95c3572
NeaguLorena <lorerocks@yahoo.com>
2020-04-05 00:24:07 +0300

    added documentation and final improvements

--- WasteLess/src/main/java/com/assignment1/wasteless/Presentation/Model/GroceryListItem.java
+++ WasteLess/src/main/java/com/assignment1/wasteless/Presentation/Model/GroceryListItem.java
@@ -7,10 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.Future;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.*;
 import java.util.Date;
 
 @Entity(name = "ITEM")
